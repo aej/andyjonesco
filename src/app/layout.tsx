fea360a28013d6cee00a5eaefd284f91a5d4250c
Andy Jones <andy@andyjones.co>
2023-08-05 12:01:08 +0100

    Try again adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import NavLink from "@/components/nav-link";
 import "./globals.css";
 import type { Metadata } from "next";
 import Head from "next/head"
+import Script from 'next/script'
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -22,9 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Head>
-     <script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="4ebc0745-db43-4ae9-9323-15fdb4d9c96d" async></script>
-      </Head>
+        <Script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="4ebc0745-db43-4ae9-9323-15fdb4d9c96d" async />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <main className="p-4 max-w-3xl mx-auto">
             <div className="mt-5 md:mt-10 flex justify-between">
