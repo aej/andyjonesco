5723dd6f9058dc7c87b62dc926a89ba213218c3c
Andy Jones <andy@andyjones.co>
2023-08-27 10:39:28 +0100

    try adding saasbanner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import NavLink from "@/components/nav-link";
 import "./globals.css";
 import type { Metadata } from "next";
-import Head from "next/head"
-import Script from 'next/script'
+import Head from "next/head";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -23,7 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="4ebc0745-db43-4ae9-9323-15fdb4d9c96d" async />
+        <Script
+          src="https://beamanalytics.b-cdn.net/beam.min.js"
+          data-token="4ebc0745-db43-4ae9-9323-15fdb4d9c96d"
+          async
+        />
+        <Script src="https://app.saasbanner.com/static/banner.js" defer />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <main className="p-4 max-w-3xl mx-auto">
             <div className="mt-5 md:mt-10 flex justify-between">
