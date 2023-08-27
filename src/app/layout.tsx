0461f4b29c64951bfb9f9eb4a4c35fe754c2fa8d
Andy Jones <andy@andyjones.co>
2023-08-27 11:07:26 +0100

    try strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
           data-token="4ebc0745-db43-4ae9-9323-15fdb4d9c96d"
           async
         />
-        <Script src="https://app.saasbanner.com/static/banner.js" />
+        <Script
+          src="https://app.saasbanner.com/static/banner.js"
+          strategy="beforeInteractive"
+        />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <main className="p-4 max-w-3xl mx-auto">
             <div className="mt-5 md:mt-10 flex justify-between">
